846fb1283820484ce94069c192c841d17286d0a0
Lion2517 <lion2517@yahoo.com>
2025-11-23 13:34:38 +0500

    Исправил подчёркивание названия страницы в "шапке"

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,30 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 
 <header style="padding-top: 50px" class="container">
-        <span class="logo poppins-extrabold">logo</span>
-        <nav>
-            <ul>
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/about.php">About us</a></li>
-                <li><a href="/portfolio.php">Portfolio</a></li>
-                <li><a href="#">News</a></li>
-                <li class="btn"><a href="/contacts.php">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+    <span class="logo poppins-extrabold">logo</span>
+    <nav>
+        <ul>
+            <li class="<?= $current_page == 'index.php' ? 'active' : '' ?>">
+                <a href="/">Home</a>
+            </li>
+
+            <li class="<?= $current_page == 'about.php' ? 'active' : '' ?>">
+                <a href="/about.php">About us</a>
+            </li>
+
+            <li class="<?= $current_page == 'portfolio.php' ? 'active' : '' ?>">
+                <a href="/portfolio.php">Portfolio</a>
+            </li>
+
+            <li>
+                <a href="#">News</a>
+            </li>
+
+            <li class="btn">
+                <a href="/contacts.php">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
